4705347abc19e43d636e72270fffce21cde3d720
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-10-10 11:02:42 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-    $db = mysqli_connect("localhost","root","","blog");
+    $db = mysqli_connect("fdb1029.awardspace.net","4232155_buzbirdph10","buzzbirdph10","blog");
     if(mysqli_connect_error()){
         echo "failed to connect".mysqli_connect_error();
     }
